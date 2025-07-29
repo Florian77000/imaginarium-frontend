ffa0447ae2dc3e0111a4acf538f12b713afdb26e
Florian77000 <floriandelaunay75@gmail.com>
2025-07-29 15:30:38 +0200

    ajout du carroussel + css / centrer, bouton dans l'image

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,14 @@
 import styles from '../styles/Home.module.css';
 import Header from "./header/header";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation,Pagination, Scrollbar, A11y } from 'swiper/modules';
 
-function Home() {
+import 'swiper/css';
+import 'swiper/css/navigation'; //Ajoute les boutons pour naviguer
+import 'swiper/css/scrollbar'; //Ajoute la navigation par scroll
+import 'swiper/css/pagination'; //Ajoute les points de la navigation
+
+export default function Home() {
   return (
     <>
       <Header />
@@ -9,13 +16,24 @@ function Home() {
         <h1 className={styles.title}>
           Welcome to l'imaginarium
         </h1>
-        <div className={styles.carroussel}>
-          <img src='./femme.jpg'></img>
-          <button className={styles.btn}>En savoir plus</button>
+        <div className={styles.swiperDiv}>
+        <Swiper
+          className={styles.swiper}
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+        >
+          <SwiperSlide><img src='./femme.jpg'></img></SwiperSlide>
+          <SwiperSlide><img src='./bracelet.jpg'></img></SwiperSlide>
+          <SwiperSlide><img src='./perle.jpg'></img></SwiperSlide>
+        </Swiper> 
         </div>
       </main>
+
+      
       </>
   );
 }
 
-export default Home;
