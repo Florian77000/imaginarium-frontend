a9a89dbe893263236e672917018bebb4edd08a38
Florian77000 <floriandelaunay75@gmail.com>
2025-07-29 21:09:38 +0200

    ajout du nom des liens pour la navbar + ajout du fonteawesome + css supression point liste navbar

--- components/header/navbar.js
+++ components/header/navbar.js
@@ -1,13 +1,20 @@
 import styles from '../../styles/Header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar () {
     return (
         <div className={styles.navbar}>
         <div className={styles.navbarLink}>
-            <li>Lien 1</li>
-            <li>Lien 2</li>
-            <li>Lien 3</li>
-            <li>Lien 4</li>
+            <li>Les bracelets</li>
+            <FontAwesomeIcon icon={faCaretDown} />
+            <li>Les colliers</li>
+            <li>Les boucles d'oreilles</li>
+            <li>Les bagues</li>
+            <li>Les mères / filles</li>
+            <li>Les best friends</li>
+            <li>Les ephemères</li>
+            <li>Les personnalisables</li>
         </div>
         </div>
     )
